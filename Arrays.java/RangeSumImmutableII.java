9b14529e35f93c7498e5d722ec073f620d177910
SARTHAK SHISHODIA <122453870+sarthakshishodia20@users.noreply.github.com>
2024-12-11 08:34:47 +0530

    Create RangeSumImmutableII.java

---
+++ Arrays.java/RangeSumImmutableII.java
@@ -0,0 +1,27 @@
+class NumMatrix {
+    int[][] matrix;
+    public NumMatrix(int[][] matrix) {
+        this.matrix = new int[matrix.length][matrix[0].length];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                this.matrix[i][j] = matrix[i][j];
+            }
+        }
+    }
+
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        int sum = 0;
+        for (int i = row1; i <= row2; i++) {
+            for (int j = col1; j <= col2; j++) {
+                sum += matrix[i][j];
+            }
+        }
+        return sum;
+    }
+}
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * NumMatrix obj = new NumMatrix(matrix);
+ * int param_1 = obj.sumRegion(row1,col1,row2,col2);
+ */
